d3d2fe385a42dd5cd008502649d1a0d79d7500c2
bruna <bruna@teste.com>
2025-10-20 10:28:46 -0300

    feat: adiciona scroll suave para links externos com hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import AboutSection from "./components/AboutSection/index.tsx";
 import HeroSection from "./components/Apresentation/index.tsx";
 import { Footer } from "./components/Footer/index.tsx";
@@ -5,9 +6,21 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects/index.tsx";
 import { Services } from "./components/ServicesOffer/index.tsx";
 import Skills from "./components/Skills/index.tsx";
-import { GlobalStyle } from './style/GlobalStyle.ts'
+import { GlobalStyle } from './style/GlobalStyle.ts';
+
 
 export default function App() {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const id = hash.replace("#", "");
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    }
+  }, []);
+
   return (
     <>
       <GlobalStyle/>
